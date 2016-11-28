012941e9ae8327c238870e1550f8a5e86ac8bdfa
XVincentX <vincenzo@apiary.io>
2016-11-28 16:28:16 +0100

    Use a template engine to set the URL

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(cors());
+app.set('view engine', 'ejs');
+app.set('views', './')
 
 app.post('/api/users/:user', (req, res) => {
   return res.json(Object.assign({
@@ -16,8 +18,10 @@ app.post('/api/users/:user', (req, res) => {
   }, req.body));
 });
 
-app.use(express.static('./', {
-  extensions: ['html']
-}));
+app.get('/serve-seed.html', (req, res) => {
+  res.render('./serve-seed.ejs', {
+    url: process.env.APIARY_SEED_URL || 'http://localhost:3000'
+  });
+});
 
 app.listen(process.env.PORT || 3001);
