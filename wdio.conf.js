94a1af97a39952d1d68b0f264dc9850ba8c2dde5
XVincentX <vincenzo@apiary.io>
2016-12-01 15:39:19 +0100

    Make the test suite executable in SauceLabs

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,18 +1,36 @@
 exports.config = {
+  protocol: 'http',
   specs: [
     '*.test.js'
   ],
   exclude: [
   ],
   capabilities: [{
-    maxInstances: 1,
-    browserName: 'chrome'
+    browserName: 'chrome',
+    version: 53
+  }, {
+    browserName: 'firefox',
+    version: 50
+  }, {
+    browserName: 'safari',
+    version: 10
+  }, {
+    browserName: 'microsoftedge',
+    version: 14
   }],
+  services: ['sauce'],
+  user: process.env.SAUCE_USERNAME,
+  key: process.env.SAUCE_ACCESS_KEY,
+  sauceConnect: true,
+  sauceConnectOpts: {
+    tunnelIdentifier: 'apiary-console-seed-tunnel',
+    dns: '8.8.8.8'
+  },
   sync: true,
   logLevel: 'silent',
   coloredLogs: true,
   screenshotPath: './errorShots/',
-  baseUrl: process.env.CI ? 'http://apiary-console.surge.sh:3000' : 'http://localhost:3000',
+  baseUrl: 'http://localhost:3000',
   waitforTimeout: 10000,
   connectionRetryTimeout: 90000,
   connectionRetryCount: 3,
