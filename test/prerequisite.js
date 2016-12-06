96087e17059718694e9b3ba2ae260ce9b2859477
XVincentX <vincenzo@apiary.io>
2016-12-06 15:37:42 +0100

    Reword the test as a prerequisite one

--- test/prerequisite.js
+++ test/prerequisite.js
@@ -1,22 +1,24 @@
-describe('The most basic test', () => {
-  it('http click', () => {
-    browser.url('/');
-    browser.leftClick('.httpCall');
-    browser.waitForExist('.detail_headers');
-    browser.waitForExist('.detail_body');
+describe('Prerequisites', () => {
+  describe('Call the same resource with both http client and iframe', () => {
+      it('http headers should be less or equal than iframe ones', () => {
+        browser.url('/');
+        browser.leftClick('.httpCall');
+        browser.waitForExist('.detail_headers');
+        browser.waitForExist('.detail_body');
 
-    const httpHeadersCount = Object.keys(browser.elements('.detail_headers').value).length;
-    const httpBodyCount = Object.keys(browser.elements('.detail_body').value).length;
+        const httpHeadersCount = Object.keys(browser.elements('.detail_headers').value).length;
+        const httpBodyCount = Object.keys(browser.elements('.detail_body').value).length;
 
-    browser.leftClick('.iframeCall');
-    browser.waitForExist('.detail_headers');
-    browser.waitForExist('.detail_body');
+        browser.leftClick('.iframeCall');
+        browser.waitForExist('.detail_headers');
+        browser.waitForExist('.detail_body');
 
-    const iframeHeadersCount = Object.keys(browser.elements('.detail_headers').value).length;
-    const iframeBodyCount = Object.keys(browser.elements('.detail_body').value).length;
+        const iframeHeadersCount = Object.keys(browser.elements('.detail_headers').value).length;
+        const iframeBodyCount = Object.keys(browser.elements('.detail_body').value).length;
 
-    expect(iframeHeadersCount).toBeGreaterThanOrEqual(httpHeadersCount);
-    expect(iframeBodyCount).toBeGreaterThanOrEqual(httpBodyCount);
+        expect(iframeHeadersCount).toBeGreaterThanOrEqual(httpHeadersCount);
+        expect(iframeBodyCount).toBeGreaterThanOrEqual(httpBodyCount);
 
-  });
+      });
+  })
 });
