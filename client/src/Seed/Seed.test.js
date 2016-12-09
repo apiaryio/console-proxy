f7cb4d754db579caca4934a69e2868a5dce3aa7a
XVincentX <vincenzo@apiary.io>
2016-12-09 14:24:13 +0100

    Crashing test part 1

--- client/src/Seed/Seed.test.js
+++ client/src/Seed/Seed.test.js
@@ -2,7 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Seed from './Seed';
 
-it('renders without crashing', () => {
+describe('if I perform a request without the seed', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<Seed />, div);
+  const seed = ReactDOM.render(<Seed scope="apiary" baseUrl="apiary" />, div);
+
+  it('it fails and I get a CHANNEL_NOT_READY error', () => {
+    seed.request({}).catch((err) => {
+      expect(err.message).toBeDefined();
+      expect(err.message).toBe('CHANNEL_NOT_READY');
+    });
+  });
 });
