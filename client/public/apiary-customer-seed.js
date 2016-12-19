d06cb3331c8c66e4bf2587b228e38cb6df3da668
XVincentX <vincenzo@apiary.io>
2016-12-19 11:43:17 +0100

    Create a buildChannel factory to make it more testable

--- client/public/apiary-customer-seed.js
+++ client/public/apiary-customer-seed.js
@@ -1,14 +1,16 @@
 axios.defaults.validateStatus = () => true;
 
-const chan = Channel.build({
-  window: window.parent,
-  origin: "*",
-  scope: "apiary-console"
-});
+function buildChannel(origin, scope) {
+  const chan = Channel.build({
+    window: window.parent,
+    origin: origin,
+    scope: scope
+  });
 
-chan.bind('httpRequest', (trans, requestOptions) => {
-  trans.delayReturn(true);
+  chan.bind('httpRequest', (trans, requestOptions) => {
+    trans.delayReturn(true);
 
-  axios(requestOptions)
-    .then(trans.complete, trans.error)
-})
+    axios(requestOptions)
+      .then(trans.complete, trans.error)
+  })
+}
