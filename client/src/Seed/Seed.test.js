6f9b18ca2ac19d13c3005283387543ecd034523a
XVincentX <vincenzo@apiary.io>
2017-01-04 17:38:29 +0100

    Write some more tests

--- client/src/Seed/Seed.test.js
+++ client/src/Seed/Seed.test.js
@@ -1,19 +1,16 @@
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000000;
+
+import fs from 'fs';
+import http from 'http';
+import path from 'path';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Seed from './Seed';
 
-import http from 'http';
-import fs from 'fs';
-
 const server = require('../../../index');
-const s2 = http.createServer((req, res) => {
-  file = fs.readFileSync('../public/apiary-customer-seed.js', 'utf8')
-  res.writeHead(200);
-  res.write(file, 'utf8');
-  res.end();
-});
+let seedServer = null;
 
-server.start();
 const ensureChannelNotReadyError = (seed) => {
   it('and if I try to call request something I get a CHANNEL_NOT_READY error', () => {
     return seed.request({}).then(undefined, (err) => {
@@ -24,23 +21,36 @@ const ensureChannelNotReadyError = (seed) => {
   });
 }
 
+beforeAll((done) => {
+  seedServer = http.createServer((req, res) => {
+    const seedFile = fs.readFileSync(path.join(__dirname, '../../public/apiary-customer-seed.js'), 'utf8');
+    res.writeHead(200, {
+      'Content-Type': 'application/javascript'
+    });
+    res.write(seedFile, 'utf8');
+    res.end();
+  }).listen(3000, 'localhost');
+  server.start(done);
+});
+
+afterAll((done) => {
+  setTimeout(() => {
+    server.stop(() => {
+      seedServer.close();
+      done()
+    })
+  }, 10000);
+});
+
 describe('if I perform a request with an non existend seed page', () => {
   const div = document.createElement('div');
   const seed = ReactDOM.render(<Seed scope="apiary" debugOutput={true} seedUrl="https://apiary.xyz" />, div);
-
   ensureChannelNotReadyError(seed);
 });
 
 describe('if I try to create a seed with invalid origin', () => {
   const div = document.createElement('div');
   const seed = ReactDOM.render(<Seed scope="apiary" debugOutput={true} seedUrl="http://localhost:3001/serve-seed.html" origin="invalidOrigin" />, div);
-
-  ensureChannelNotReadyError(seed);
-});
-
-describe('Nasino pariosino', () => {
-  const div = document.createElement('div');
-  const seed = ReactDOM.render(<Seed scope="apiary" seedUrl="http://localhost:3001/serve-seed.html" origin="*" />, div);
 
   ensureChannelNotReadyError(seed);
 });
