be54534929ea66a16ee00e4dfe69d1a9e098ea6b
XVincentX <vincenzo@apiary.io>
2017-04-20 15:35:38 +0200

    Add createAgent utility method

--- client/karma.iframe.conf.js
+++ client/karma.iframe.conf.js
@@ -1,15 +1,19 @@
 process.env.NODE_ENV = 'test';
 const path = require('path');
+const DefinePlugin = require('webpack/lib/DefinePlugin')
 
 var webpackConfig = require(path.join(__dirname, './node_modules/react-scripts/config/webpack.config.dev.js'));
 webpackConfig.devtool = 'inline-source-map';
+webpackConfig.plugins.push(new DefinePlugin({
+  DOMAIN: JSON.stringify('apiary.dev')
+}));
 
 
 module.exports = function (config) {
   config.set({
     basePath: '',
     frameworks: ['jasmine'],
-    files: ['src/**/iframe/**/**.test.js'],
+    files: ['src/**/iframe/**/**.test.js', 'src/**/JsAgent/**/**.test.js'],
     exclude: [],
     preprocessors: { 'src/**/**.test.js': ['webpack', 'sourcemap'] },
     webpack: webpackConfig,
